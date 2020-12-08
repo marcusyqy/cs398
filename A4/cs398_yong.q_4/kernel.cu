01a88b80fdc2f6563ed18037c97d2a4ac11d5137
marcusyqy <50792280+marcusyqy@users.noreply.github.com>
2020-12-08 17:42:44 +0800

    fiels

--- A4/cs398_yong.q_4/kernel.cu
+++ A4/cs398_yong.q_4/kernel.cu
@@ -18,30 +18,87 @@
 __global__ void histogram(	unsigned char *input, 
 							unsigned int *output,
 							int width, 
-							int height) {
+							int height) 
+{
+	__shared__ unsigned int pHistogram[HISTOGRAM256_BIN_COUNT];
+
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	int wb = threadIdx.y * BLOCK_SIZE + threadIdx.x;
+
+	if (wb < HISTOGRAM256_BIN_COUNT) {
+		pHistogram[wb] = 0;
+	}
+	__syncthreads();
+
+	if(x < width && y < height) {
+		int index = y * width + x;
+		unsigned char v = (unsigned char)input[index];
+		atomicAdd(&pHistogram[(int)v],1);
+	}
+
+	__syncthreads();
+		
+	if(wb < HISTOGRAM256_BIN_COUNT) {
+		atomicAdd(&output[wb], pHistogram[wb]);
+	}
 
 }
 __global__ void cdfScan(unsigned int *input, 
 						float *output,
 						float width, 
-						float height) {
+						float height) 
+{
+	__shared__ unsigned int pHistogram[HISTOGRAM256_BIN_COUNT];
 
-	for (unsigned int stride = blockDim.x; stride > 0;  stride /= 2) {
+    //int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+	pHistogram[threadIdx.x] = input[threadIdx.x]; //PROBABILITY((float)input[threadIdx.x], width, height);
+	pHistogram[threadIdx.x + blockDim.x] = input[threadIdx.x + blockDim.x]; //PROBABILITY((float)input[threadIdx.x + blockDim.x], width, height);
+	
+	for (unsigned int stride = 1;stride <=  blockDim.x; stride *= 2) {
+		int index = (threadIdx.x+1)*stride*2 - 1;
+		if(index < 2* blockDim.x)
+		pHistogram[index] += pHistogram[index-stride];
 		__syncthreads();
-		if (t < stride)
-			partialSum[t] += partialSum[t+stride];
 	}
+
+	__syncthreads();
+
+	for (unsigned int stride =  blockDim.x /2; stride > 0; stride /= 2) {
+		__syncthreads();
+		int index = (threadIdx.x+1)*stride*2 - 1;
+		if(index+stride < 2*  blockDim.x) {
+			pHistogram[index + stride] += pHistogram[index];
+		}
+	}
+	__syncthreads();
+
+	float max_value = pHistogram[HISTOGRAM256_BIN_COUNT - 1];
+	float cdfMin = pHistogram[0]/max_value;
+
+	output[threadIdx.x] =  (float)CORRECT_COLOR(pHistogram[threadIdx.x]/max_value, cdfMin);
+	output[threadIdx.x + blockDim.x] = (float)CORRECT_COLOR(pHistogram[threadIdx.x + blockDim.x]/max_value, cdfMin);
 }
 
 
 __global__ void applyHistogram(	unsigned char *input1, 
 								float *input2,
-								float *input3, 
 								unsigned char *output,
 								int width, 
 								int height, 
 								int channels) 
 {
+	
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+	if(x < width && y < height) {
+		int index = width * y + x;
+		output[index] = (unsigned char)input2[(int)input1[index]];
+	}
+	
 }
 ////////////////////////////////////////////////////////////////////////////////
 // Host interface to GPU histogram
@@ -79,21 +136,38 @@ extern "C" void histogram256(
 {
 	for (uint i = 0; i < imgChannels; ++i) { 
 		
+		uint offset = i * imgWidth* imgHeight;
+		uint hist_offset = i * HISTOGRAM256_BIN_COUNT;
+
 		dim3 dimGrid((imgWidth-1) / BLOCK_SIZE + 1, (imgHeight-1) / BLOCK_SIZE + 1, imgChannels);
 		dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
 
 		histogram<<<dimGrid, dimBlock>>>(
-			(float*)d_DataIn, (unsigned int*)d_Histogram, (int)imgWidth, (int)imgHeight
+			(unsigned char*)d_DataIn + offset, (unsigned int*)d_Histogram + hist_offset, (int)imgWidth, (int)imgHeight
 		);
 
-		dim3 dimGrid((HISTOGRAM256_BIN_COUNT-1) / BLOCK_SIZE + 1, (HISTOGRAM256_BIN_COUNT-1) / BLOCK_SIZE + 1, imgChannels);
-		dim3 dimBlock(BLOCK_SIZE, BLOCK_SIZE, 1);
-		
+		checkCudaErrors(cudaDeviceSynchronize());
+
+		dim3 dimGridCdf(1,1,1);
+		dim3 dimBlockCdf(HISTOGRAM256_BIN_COUNT/2, 1, 1);
+
+		cdfScan<<<dimGridCdf,dimBlockCdf>>>(
+			(unsigned int*)d_Histogram + hist_offset,
+			(float*)histogramCdf,
+			(float)imgWidth, (float)imgHeight
+		);
+
 		//cdfScan
-
-
-
-
+		checkCudaErrors(cudaDeviceSynchronize());
+		
+		applyHistogram <<<dimGrid, dimBlock >> > (
+			(unsigned char*)d_DataIn + offset,
+			(float*)histogramCdf,
+			(unsigned char*)d_DataOut + offset,
+			(int)imgWidth, (int)imgHeight, (int)imgChannels		
+		);
+		
+		checkCudaErrors(cudaDeviceSynchronize());
 	}
 
 }
